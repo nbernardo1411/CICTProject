e60716fe6b4832a827757fc3835778069caee062
Zuki <113109523+Zukito1411@users.noreply.github.com>
2023-04-17 01:52:34 +0800

    minor ups

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -17,11 +17,11 @@ interface Schedule {
 })
 export class ScheduleComponent  {
   @ViewChild('scheduleTable') scheduleTable!: ElementRef;
-
+    facultyList: string[] = ['proxy data 1','proxy data 2'];
+    facultyName: string = '';
 
   days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
   timeSlots = ['7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM'];
-  facultyName: string = '';
   schedule: Schedule = {
     monday: {},
     tuesday: {},

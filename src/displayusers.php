ac7d4c08ae0688b3b204a529bd46167a58722fe7
Zuki <113109523+Zukito1411@users.noreply.github.com>
2023-04-26 01:18:19 +0800

    up

---
+++ src/displayusers.php
@@ -0,0 +1,40 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
+
+// Create connection to MySQL database
+$conn = new mysqli('localhost', 'root', '', 'cict_scheduling_key_inv');
+
+// Check if connection succeeded
+if ($conn->connect_error) {
+  die('Connection failed: ' . $conn->connect_error);
+}
+
+// Retrieve all users
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  $query = "SELECT * FROM users";
+  $result = $conn->query($query);
+
+  if ($result->num_rows > 0) {
+    $users = [];
+
+    while ($row = $result->fetch_assoc()) {
+      $users[] = $row;
+    }
+
+    $response = [
+      'success' => true,
+      'data' => $users,
+    ];
+  } else {
+    $response = [
+      'success' => false,
+      'message' => 'No users found.',
+    ];
+  }
+
+  // Send response to client
+  echo json_encode($response);
+}
+?>

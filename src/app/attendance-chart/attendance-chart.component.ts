2ccd5256b41294acaece03b535c12a23208753bb
Zuki <113109523+Zukito1411@users.noreply.github.com>
2023-04-28 19:36:33 +0800

    up

--- src/app/attendance-chart/attendance-chart.component.ts
+++ src/app/attendance-chart/attendance-chart.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart, ChartConfiguration, ChartType } from 'chart.js/auto';
+import { HttpClient } from '@angular/common/http';
 
-
+interface AttendanceData {
+  present: number;
+  absent: number;
+}
 
 @Component({
   selector: 'app-attendance-chart',
@@ -10,30 +14,35 @@ import { Chart, ChartConfiguration, ChartType } from 'chart.js/auto';
 export class AttendanceChartComponent implements OnInit {
   attendanceChart: any;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     const ctx = document.getElementById('attendance-chart') as HTMLCanvasElement | null;
     if (ctx) {
+      this.fetchAttendanceData().subscribe((data: AttendanceData) => {
+        const config: ChartConfiguration<ChartType, number[], string> = {
+          type: 'doughnut',
+          data: {
+            labels: ['Present', 'Absent'],
+            datasets: [
+              {
+                data: [data.present, data.absent],
+                backgroundColor: ['#00ff40', '#ff0000'],
+              },
+            ],
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+          },
+        };
 
-      const config: ChartConfiguration<ChartType, number[], string> = {
-        type: 'doughnut',
-        data: {
-          labels: ['Present', 'Absent'],
-          datasets: [
-            {
-              data: [10, 1],
-              backgroundColor: ['#00ff40', '#ff0000'],
-            },
-          ],
-        },
-        options: {
-          responsive: true,
-          maintainAspectRatio: false,
-        },
-      };
-
-      this.attendanceChart = new Chart(ctx, config);
+        this.attendanceChart = new Chart(ctx, config);
+      });
     }
+  }
+
+  private fetchAttendanceData() {
+    return this.http.get<AttendanceData>('http://localhost/CICTProject/src/get-attendance-data.php');
   }
 }
---
+++ src/get-attendance-data.php
@@ -0,0 +1,36 @@
+<?php
+header('Content-Type: application/json');
+
+// Create connection to MySQL database
+$conn = new mysqli('localhost', 'root', '', 'cict_scheduling_key_inv');
+
+// Check for errors
+if ($conn->connect_error) {
+  die('Connection failed: ' . $conn->connect_error);
+}
+
+// Query database for attendance data
+$sql = "SELECT COUNT(*) as count, status FROM attendance GROUP BY status";
+$result = $conn->query($sql);
+
+// Check for errors
+if (!$result) {
+  die('Error: ' . $sql . '<br>' . $conn->error);
+}
+
+// Build attendance data object
+$attendance_data = array('present' => 0, 'absent' => 0);
+while ($row = $result->fetch_assoc()) {
+  if ($row['status'] == 'present') {
+    $attendance_data['present'] = (int) $row['count'];
+  } else if ($row['status'] == 'absent') {
+    $attendance_data['absent'] = (int) $row['count'];
+  }
+}
+
+// Close the database connection
+$conn->close();
+
+// Send attendance data as JSON response
+echo json_encode($attendance_data);
+?>
